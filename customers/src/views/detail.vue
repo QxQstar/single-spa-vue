3b38d9b037092f4be22f10e0baa96e02b5472efa
何遇 <1395294694@qq.com>
2019-10-09 11:53:42 +0800

    feat: 客户管理项目

---
+++ customers/src/views/detail.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="about">
+    <router-link to="/">返回</router-link>
+    <div class="msg">
+      <img src="https://avatars1.githubusercontent.com/u/17845091?s=460&v=4">
+
+      <div class="detail">
+        <p class="title"> 基本信息</p>
+        <p>姓名：何遇</p>
+        <p>性别：女</p>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<style>
+  .msg{
+    margin-top: 30px;
+  }
+</style>
+
---
+++ customers/src/views/list.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="home">
+    <div class="item" @click="gotoDetail">
+      <img src="https://avatars1.githubusercontent.com/u/17845091?s=460&v=4"/>
+      <p>何遇</p>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'home',
+  methods:{
+    gotoDetail(){
+      this.$router.push('/detail')
+    }
+  }
+}
+</script>
+
+
+<style lang="less" scoped>
+  .item{
+    width: 200px;
+    box-shadow: 0 0 20px #000;
+    text-align: center;
+    img{
+      max-width: 100%;
+    }
+
+  }
+
+</style>
+
