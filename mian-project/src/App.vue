bf1a9459c49b9df656a4650c6ccf5054cbc97a67
何遇 <1395294694@qq.com>
2019-10-09 11:54:48 +0800

    feat: 微前端项目基座

---
+++ mian-project/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/about">关于</router-link> |
+      <router-link to="/other/goods">商品管理</router-link> |
+      <router-link to="/other/customers">客户管理</router-link>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<style lang="less">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+#nav {
+  padding: 30px;
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
---
+++ mian-project/src/router.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from './views/Home.vue'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      component: Home
+    },
+    {
+      path:'/about',
+      component:() => import('./views/About.vue')
+    },
+    {
+      path:'/other/:project',
+      name:'otherProject',
+      component:() => import('./views/otherProject.vue')
+    }
+  ]
+})
