fa416b6363f941fd89776d576d2e72df99773487
何遇 <1395294694@qq.com>
2019-10-23 11:35:27 +0800

    feat: 确保主项目装载到页面上才注册子项目

--- main/single-spa.config.js
+++ main/single-spa.config.js
@@ -2,7 +2,7 @@
 function isActive(location,page) {
     const hash = location.hash.slice(2),
         hashArr = hash.split('/');
-    
+
     return hashArr[0] === page;
 }
 const activeFns = {
@@ -17,18 +17,27 @@ const activeFns = {
     }
 }
 
+function registerSub (singleSpa) {
+    // 确保主项目已经装载才注册子项目
+    if(document && document.getElementById('main')) {
+        // 商品管理
+        singleSpa.registerApplication('customers',() => System.import('customers'),activeFns.customer);
+        // 商品管理
+        singleSpa.registerApplication('goods',() => System.import('goods'),activeFns.goods);
+    } else {
+        setTimeout(function () {
+            registerSub(singleSpa);
+        },50)
+    }
 
+}
 // 注册应用
 Promise.all([System.import('single-spa'),System.import('vue')]).then(modules => {
     const singleSpa = modules[0];
     // 主项目要一直显示
     singleSpa.registerApplication('main-project',() => System.import('main-project'),activeFns.main)
-
-    // 商品管理
-    singleSpa.registerApplication('customers',() => System.import('customers'),activeFns.customer);
-    // 商品管理
-    singleSpa.registerApplication('goods',() => System.import('goods'),activeFns.goods);
-
+    // 注册子项目
+    registerSub(singleSpa);
     singleSpa.start();
 })
 
