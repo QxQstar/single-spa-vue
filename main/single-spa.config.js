cfe27164f43490439adfb774fdd4b6bd4af338e5
何遇 <1395294694@qq.com>
2019-11-06 15:09:57 +0800

    feat: 给singele-spa的applicationOrLoadingFn包装一层

--- main/single-spa.config.js
+++ main/single-spa.config.js
@@ -19,17 +19,44 @@ function bootstrap() {
         singleSpa.start();
     })
 }
+function genSandbox(appName) {
 
-
-
+    return {
+        sandbox:{},
+        sandboxMount(){
+            return Promise.resolve();
+        },
+        sandboxUnmount(){
+            return Promise.resolve()
+        }
+    }
+}
+function genAppLoadPromise(appPromise,appName) {
+    return appPromise.then((app) => {
+                const Sandbox = genSandbox(appName);
+                console.log(Sandbox,'ssss')
+                return {
+                    bootstrap:[app.bootstrap],
+                    mount:[app.mount,Sandbox.sandboxUnmount],
+                    unmount:[app.unmount,Sandbox.sandboxUnmount]
+                };
+            })
+}
 // 注册项目
 function registerApp(singleSpa,projects) {
     projects.forEach(function (project) {
         function start(app) {
             // 确保应用挂载点在页面中存在
             if(!app.domID || document.getElementById(app.domID)) {
+                singleSpa.registerApplication(project.name,
+                    () => {
+                        const data = System.import(project.main);
+                        // 只要data是一个promise并且promise resolve的值是一个对象，对象必须包含bootstrap，mount，unmount
 
-                singleSpa.registerApplication(project.name,() => System.import(project.main),project.base ? (function () { return true }) : activeFns(project),{name:'fdfd'})
+                        // bootstrap，mount，unmount是函数，并且函数返回promise
+                        return genAppLoadPromise(data,project.name)
+                    },
+                    project.base ? (function () { return true }) : activeFns(project))
             } else {
                 setTimeout(function () {
                     start(app);
